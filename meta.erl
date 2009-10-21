70925c8354aeaf6df7ef2d4a31ea06c8a5e9e88f
Jesse Andrews <anotherjesse@gmail.com>
2009-10-21 01:40:27 -0700

    prototyping

--- meta.erl
+++ meta.erl
@@ -1,5 +1,4 @@
--module(s2).
-
+-module(meta).
 -behaviour(gen_server).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export([get/1,
@@ -42,15 +41,15 @@ handle_call({put, Object, Headers}, _From, State) ->
                              headers = Headers } )
           end,
     {atomic, Result} = mnesia:transaction(Fun),
-    {reply, {ok, Result}, State};
+    {reply, Result, State};
 
 handle_call({get, Object}, _From, State) ->
     Fun =
         fun() ->
                 mnesia:read({object, Object})
         end,
-    {atomic, [Row]} = mnesia:transaction(Fun),
-    {reply, {ok, Row}, State};
+    {atomic, Row} = mnesia:transaction(Fun),
+    {reply, Row, State};
 
 handle_cast(_Msg, State) -> {noreply, State}.
 handle_info(_Msg, State) -> {noreply, State}.
